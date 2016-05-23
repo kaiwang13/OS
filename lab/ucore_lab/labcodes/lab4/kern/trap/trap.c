7b12f8571a59422a3dedc3618a36123eb3310df1
s0261 <719424398@qq.com>
2016-05-23 18:02:08 +0800

    lab4

--- lab/ucore_lab/labcodes/lab4/kern/trap/trap.c
+++ lab/ucore_lab/labcodes/lab4/kern/trap/trap.c
@@ -53,9 +53,6 @@ idt_init(void) {
     for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++) {
         SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
     }
-	// set for switch from user to kernel
-    SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
-	// load the IDT
     lidt(&idt_pd);
 }
 
