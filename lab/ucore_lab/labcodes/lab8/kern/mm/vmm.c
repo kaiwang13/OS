82f014e7b3bde1bd50f977ded1b2f2d6d127c806
s0261 <719424398@qq.com>
2016-05-25 20:25:21 +0800

    lab8

--- lab/ucore_lab/labcodes/lab8/kern/mm/vmm.c
+++ lab/ucore_lab/labcodes/lab8/kern/mm/vmm.c
@@ -493,7 +493,33 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
         }
    }
 #endif
-   ret = 0;
+    if ((ptep = get_pte(mm->pgdir, addr, 1)) == NULL) {
+        cprintf("get_pte in do_pgfault failed\n");
+        return ret;
+    }
+    if (*ptep == 0) {
+        if (pgdir_alloc_page(mm->pgdir, addr, perm) == NULL) {
+            cprintf("pgdir_alloc_page in do_pgfault failed\n");
+            return ret;
+        }
+    }
+    else {
+        if(swap_init_ok) {
+            struct Page *page=NULL;
+            if ((ret = swap_in(mm, addr, &page)) != 0) {
+                cprintf("swap_in in do_pgfault failed\n");
+                return ret;
+            }    
+            page_insert(mm->pgdir, page, addr, perm);
+            swap_map_swappable(mm, addr, page, 1);
+            page->pra_vaddr = addr;
+        }
+        else {
+            cprintf("no swap_init_ok but ptep is %x, failed\n",*ptep);
+            return ret;
+        }
+    }
+    return 0;
 failed:
     return ret;
 }
